ad5898a20eeacdc3c0e198889eb04e246577bf77
HexXxaN <85108919+HexXxaN@users.noreply.github.com>
2021-06-14 21:32:00 +0200

    Update MyForm.h

--- MyForm.h
+++ MyForm.h
@@ -15,9 +15,6 @@ namespace CompressionTool {
 	using namespace System::Drawing;
 	using namespace System::IO;
 
-	/// <summary>
-	///
-	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
@@ -27,9 +24,6 @@ namespace CompressionTool {
 		}
 
 	protected:
-		/// <summary>
-		/// 
-		/// </summary>
 		~MyForm()
 		{
 			if (components)
@@ -45,21 +39,10 @@ namespace CompressionTool {
 	private: System::ComponentModel::BackgroundWorker^ backgroundWorker1;
 	private: System::ComponentModel::BackgroundWorker^ backgroundWorker2;
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Label^ label2;
-
-
-
 	private:
-		/// <summary>
-		/// Wymagana zmienna projektanta.
-		/// </summary>
 		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// 
-		///
-		/// </summary>
 		void InitializeComponent(void)
 		{
 			this->button_to_compress = (gcnew System::Windows::Forms::Button());
@@ -68,11 +51,9 @@ namespace CompressionTool {
 			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
 			this->backgroundWorker2 = (gcnew System::ComponentModel::BackgroundWorker());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
+			
 			this->SuspendLayout();
-			// 
-			// button_to_compress
-			// 
+			
 			this->button_to_compress->Location = System::Drawing::Point(12, 12);
 			this->button_to_compress->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->button_to_compress->Name = L"button_to_compress";
@@ -81,9 +62,7 @@ namespace CompressionTool {
 			this->button_to_compress->Text = L"Compress file";
 			this->button_to_compress->UseVisualStyleBackColor = true;
 			this->button_to_compress->Click += gcnew System::EventHandler(this, &MyForm::button_to_compress_Click);
-			// 
-			// button_to_decompress
-			// 
+			
 			this->button_to_decompress->Location = System::Drawing::Point(12, 78);
 			this->button_to_decompress->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->button_to_decompress->Name = L"button_to_decompress";
@@ -92,9 +71,7 @@ namespace CompressionTool {
 			this->button_to_decompress->Text = L"Decompress file";
 			this->button_to_decompress->UseVisualStyleBackColor = true;
 			this->button_to_decompress->Click += gcnew System::EventHandler(this, &MyForm::button_to_decompress_Click);
-			// 
-			// progressBar
-			// 
+			
 			this->progressBar->Location = System::Drawing::Point(13, 144);
 			this->progressBar->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->progressBar->MarqueeAnimationSpeed = 20;
@@ -102,45 +79,25 @@ namespace CompressionTool {
 			this->progressBar->Size = System::Drawing::Size(557, 62);
 			this->progressBar->Style = System::Windows::Forms::ProgressBarStyle::Continuous;
 			this->progressBar->TabIndex = 3;
-			// 
-			// backgroundWorker1
-			// 
+			
 			this->backgroundWorker1->DoWork += gcnew System::ComponentModel::DoWorkEventHandler(this, &MyForm::backgroundWorker1_DoWork);
 			this->backgroundWorker1->RunWorkerCompleted += gcnew System::ComponentModel::RunWorkerCompletedEventHandler(this, &MyForm::backgroundWorker1_RunWorkerCompleted);
-			// 
-			// backgroundWorker2
-			// 
+			
 			this->backgroundWorker2->DoWork += gcnew System::ComponentModel::DoWorkEventHandler(this, &MyForm::backgroundWorker2_DoWork);
 			this->backgroundWorker2->RunWorkerCompleted += gcnew System::ComponentModel::RunWorkerCompletedEventHandler(this, &MyForm::backgroundWorker2_RunWorkerCompleted);
-			// 
-			// label1
-			// 
+			
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(200, 65);
+			this->label1->Location = System::Drawing::Point(197, 65);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(253, 32);
+			this->label1->Size = System::Drawing::Size(303, 32);
 			this->label1->TabIndex = 4;
-			this->label1->Text = L"Compression ratio:";
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(455, 65);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(65, 32);
-			this->label2->TabIndex = 5;
-			this->label2->Text = L"-- %";
-			// 
-			// MyForm
-			// 
+			this->label1->Text = L"Compression ratio: --%";
+			
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(582, 218);
-			this->Controls->Add(this->label2);
+			this->ClientSize = System::Drawing::Size(581, 218);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->progressBar);
 			this->Controls->Add(this->button_to_decompress);
@@ -149,9 +106,9 @@ namespace CompressionTool {
 			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->Name = L"MyForm";
 			this->Text = L"CompressionTool";
+			this->Activated += gcnew System::EventHandler(this, &MyForm::MyForm_Activated);
 			this->ResumeLayout(false);
 			this->PerformLayout();
-
 		}
 #pragma endregion
 	private: System::Void button_to_compress_Click(System::Object^ sender, System::EventArgs^ e)
@@ -160,13 +117,13 @@ namespace CompressionTool {
 
 		openFileDialog1->InitialDirectory = "c:\\";
 		openFileDialog1->Filter = "txt files (*.txt)|*.txt";
-		openFileDialog1->FilterIndex = 1;
 		openFileDialog1->RestoreDirectory = true;
 
 		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
 		{
 			this->button_to_decompress->Enabled = false;
-			this->label2->Text = "--%";
+			this->button_to_compress->Enabled = false;
+			this->label1->Text = "Compression ratio: --%";
 			this->backgroundWorker1->RunWorkerAsync(openFileDialog1->FileName);
 			this->progressBar->Style = System::Windows::Forms::ProgressBarStyle::Marquee;
 		}
@@ -180,13 +137,13 @@ namespace CompressionTool {
 
 		openFileDialog2->InitialDirectory = "c:\\";
 		openFileDialog2->Filter = "huf files (*.huf)|*.huf";
-		openFileDialog2->FilterIndex = 1;
 		openFileDialog2->RestoreDirectory = true;
 
 		if (openFileDialog2->ShowDialog() == System::Windows::Forms::DialogResult::OK)
 		{
 			this->button_to_compress->Enabled = false;
-			this->label2->Text = "--%";
+			this->button_to_decompress->Enabled = false;
+			this->label1->Text = "Compression ratio: --%";
 			this->backgroundWorker2->RunWorkerAsync(openFileDialog2->FileName);
 			this->progressBar->Style = System::Windows::Forms::ProgressBarStyle::Marquee;
 		}
@@ -210,16 +167,16 @@ namespace CompressionTool {
 		tree.delete_tree(tree.root);
 
 		std::ifstream fileToCompress(directory, std::ios::binary);
-		std::ifstream compressedFile("compressed.huf", std::ios::binary);
+		std::ifstream compressedFile("c:\\CompressionToolFolder\\compressed.huf", std::ios::binary);
 		fileToCompress.seekg(0, std::ios::end);
-		unsigned long long int fileToCompressSize = fileToCompress.tellg();
+		uint64_t fileToCompressSize = fileToCompress.tellg();
 		compressedFile.seekg(0, std::ios::end);
-		unsigned long long int compressedFileSize = compressedFile.tellg();
+		uint64_t compressedFileSize = compressedFile.tellg();
 		fileToCompress.close();
 		compressedFile.close();
 
-		double ratio = (double) compressedFileSize/fileToCompressSize ;
-		ratio = std::ceil(ratio * 1000)/10;
+		double ratio = (double) compressedFileSize/fileToCompressSize;
+		ratio = std::ceil(ratio * 10000)/100;
 		e->Result = ratio;
 		
 		delete context;
@@ -228,28 +185,29 @@ namespace CompressionTool {
 	private: System::Void backgroundWorker1_RunWorkerCompleted(System::Object^ sender, System::ComponentModel::RunWorkerCompletedEventArgs^ e)
 	{
 		this->progressBar->Style = System::Windows::Forms::ProgressBarStyle::Continuous;
-		this->label2->Text = e->Result->ToString() + "%";
+		this->label1->Text = "Compression ratio: " + e->Result->ToString() + "%";
 
 		SaveFileDialog^ saveFileDialog1 = gcnew SaveFileDialog;
 
 		saveFileDialog1->InitialDirectory = "c:\\";
 		saveFileDialog1->Filter = "huf files (*.huf)|*.huf";
-		saveFileDialog1->FilterIndex = 1;
 		saveFileDialog1->RestoreDirectory = true;
 
 		if (saveFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
 		{		
 			marshal_context^ context = gcnew marshal_context();
 			const char* directory = context->marshal_as<const char*>(saveFileDialog1->FileName);
-			int result = rename("compressed.huf", directory);
+			int result = rename("c:\\CompressionToolFolder\\compressed.huf", directory);
+			
 			delete context;
 		}
 		else
-			remove("compressed.huf");
+			remove("c:\\CompressionToolFolder\\compressed.huf");
 
 		delete saveFileDialog1;
 
 		this->button_to_decompress->Enabled = true;
+		this->button_to_compress->Enabled = true;
 	}
 
 	private: System::Void backgroundWorker2_DoWork(System::Object^ sender, System::ComponentModel::DoWorkEventArgs^ e)
@@ -262,7 +220,7 @@ namespace CompressionTool {
 		directory = context->marshal_as<const char*>(dir);
 		tree.decompress_file(directory);
 		tree.delete_tree(tree.root);
-		
+
 		delete context;
 	}
 
@@ -274,22 +232,29 @@ namespace CompressionTool {
 
 		saveFileDialog2->InitialDirectory = "c:\\";
 		saveFileDialog2->Filter = "txt files (*.txt)|*.txt";
-		saveFileDialog2->FilterIndex = 1;
 		saveFileDialog2->RestoreDirectory = true;
 
 		if (saveFileDialog2->ShowDialog() == System::Windows::Forms::DialogResult::OK)
 		{
 			marshal_context^ context = gcnew marshal_context();
 			const char *directory = context->marshal_as<const char*>(saveFileDialog2->FileName);
-			int result = rename("decompressed.txt", directory);
+			int result = rename("c:\\CompressionToolFolder\\decompressed.txt", directory);
+			
 			delete context;
 		}
 		else
-			remove("decompressed.txt");
+			remove("c:\\CompressionToolFolder\\decompressed.txt");
 
 		delete saveFileDialog2;
 		
+		this->button_to_decompress->Enabled = true;
 		this->button_to_compress->Enabled = true;
+	}
+	
+private: System::Void MyForm_Activated(System::Object^ sender, System::EventArgs^ e)
+	{
+	if (!Directory::Exists("c:\\CompressionToolFolder"))
+		DirectoryInfo^ di = Directory::CreateDirectory("c:\\CompressionToolFolder");
 	}
 };
 }
